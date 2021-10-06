9bb383fa7252ea0a7325088e28b251dbabb9b3a2
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-10-06 20:35:23 +0200

    add tests for AuthAccountStore

---
+++ test/account_store_test.dart
@@ -0,0 +1,133 @@
+import 'dart:io';
+
+import 'package:fast_immutable_collections/src/ilist/list_extension.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:path/path.dart';
+import 'package:subsocial_flutter_auth/src/auth_account_store.dart';
+import 'package:subsocial_flutter_auth/src/models/auth_account.dart';
+import 'package:subsocial_flutter_auth/src/sembast_auth_account.dart';
+
+import 'mocks.dart';
+
+const _databasesDirPath = './test_data/account_store_test/';
+
+// Creates a random db for each test case
+String _dbPath() {
+  return join(_databasesDirPath, getRandomString(20));
+}
+
+void main() {
+  tearDownAll(() async {
+    final dbsDir = Directory(_databasesDirPath);
+    if (await dbsDir.exists()) {
+      await dbsDir.delete();
+    }
+  });
+
+  test('change in data will notify listeners', () async {
+    final AuthAccountStore store = SembastAuthAccountStore(_dbPath());
+    int invokedCounter = 0;
+    void _listener() => invokedCounter++;
+    store.addListener(_listener);
+    final accounts =
+        List<AuthAccount>.generate(10, (index) => generateRandomMockAccount());
+
+    await Future.wait(accounts.map((account) async {
+      await store.addAccount(account);
+    }));
+    expect(invokedCounter, 10);
+
+    await store.setActiveAccount(generateRandomMockAccount());
+    await store.unsetActiveAccount();
+
+    expect(invokedCounter, 12);
+
+    await Future.wait(accounts.map((account) async {
+      await store.removeAccount(account.publicKey);
+    }));
+    expect(invokedCounter, 22);
+
+    store.removeListener(_listener);
+  });
+
+  test('add/remove/get account', () async {
+    final AuthAccountStore store = SembastAuthAccountStore(_dbPath());
+    final accounts =
+        List<AuthAccount>.generate(10, (index) => generateRandomMockAccount());
+    await Future.wait(accounts.map((account) async {
+      await store.addAccount(account);
+    }));
+
+    final resultAccountsAfterAdd = await store.getStoredAccounts();
+    expect(resultAccountsAfterAdd.toSet(), accounts.toSet()); // ignore order
+
+    await Future.wait(accounts.map((account) async {
+      await store.removeAccount(account.publicKey);
+    }));
+
+    final resultAccountsAfterRemove = await store.getStoredAccounts();
+
+    expect(resultAccountsAfterRemove, isEmpty);
+
+    await store.addAccount(generateRandomMockAccount());
+    final afterAnotherAdd = await store.getStoredAccounts();
+    expect(afterAnotherAdd.length, 1);
+  });
+
+  test('add will override account if same public key', () async {
+    final AuthAccountStore store = SembastAuthAccountStore(_dbPath());
+    final a = await store.getStoredAccounts();
+
+    final account1 = generateRandomMockAccount();
+    final account2 = generateRandomMockAccount().copyWith(
+      publicKey: account1.publicKey,
+    );
+
+    await store.addAccount(account1);
+
+    expect(await store.getStoredAccounts(), [account1].lock);
+
+    await store.addAccount(account2);
+
+    expect(await store.getStoredAccounts(), [account2].lock);
+  });
+
+  test('get account', () async {
+    final AuthAccountStore store = SembastAuthAccountStore(_dbPath());
+    final accounts =
+        List<AuthAccount>.generate(10, (index) => generateRandomMockAccount());
+    await Future.wait(accounts.map((account) async {
+      await store.addAccount(account);
+    }));
+
+    final result = await store.getAccount(accounts[5].publicKey);
+
+    expect(result, accounts[5]);
+  });
+
+  test('active account', () async {
+    final AuthAccountStore store = SembastAuthAccountStore(_dbPath());
+    final accounts =
+        List<AuthAccount>.generate(10, (index) => generateRandomMockAccount());
+    await Future.wait(accounts.map((account) async {
+      await store.addAccount(account);
+    }));
+
+    expect(await store.getActiveAccount(), isNull);
+
+    await store.setActiveAccount(accounts[1]);
+
+    expect(await store.getActiveAccount(), accounts[1]);
+
+    await store.unsetActiveAccount();
+
+    expect(await store.getActiveAccount(), isNull);
+
+    await store.setActiveAccount(accounts[4]);
+
+    expect(await store.getActiveAccount(), accounts[4]);
+
+    // no change to accounts
+    expect((await store.getStoredAccounts()).toSet(), accounts.toSet());
+  });
+}
--- test/mocks.dart
+++ test/mocks.dart
@@ -1,7 +1,30 @@
+import 'dart:math';
+import 'dart:typed_data';
+
 import 'package:mocktail/mocktail.dart';
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:subsocial_flutter_auth/src/models/auth_account.dart';
 import 'package:subsocial_sdk/subsocial_sdk.dart';
+
+const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+Random _rnd = Random();
+
+String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
+    length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
+
+AuthAccount generateRandomMockAccount() {
+  return AuthAccount(
+    localName: getRandomString(10),
+    publicKey: getRandomString(30),
+    accountSecret: AccountSecret(
+      encryptedSuri: Uint8List(0),
+      encryptionKeySalt: Uint8List(0),
+      passwordHash: Uint8List(0),
+      passwordSalt: Uint8List(0),
+    ),
+  );
+}
 
 class MockSubsocial extends Mock implements Subsocial {}
 
