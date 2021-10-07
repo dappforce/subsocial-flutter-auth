ba3d81dde8e5d4a74ce4d486ae26c63e64167dd8
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-10-07 17:40:15 +0200

    Add library exports

--- lib/subsocial_flutter_auth.dart
+++ lib/subsocial_flutter_auth.dart
@@ -1,7 +1,10 @@
 library subsocial_flutter_auth;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/auth_account_factory.dart';
+export 'src/auth_account_store.dart';
+export 'src/crypto.dart';
+export 'src/key_derivation_strategy.dart';
+export 'src/models/auth_account.dart';
+export 'src/models/auth_state.dart';
+export 'src/sembast_auth_account_store.dart';
+export 'src/subsocial_auth.dart';
