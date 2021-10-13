8b82496fa4b3998476ba1d808a8bea94658aea8d
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-10-13 16:20:51 +0200

    add utils test

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,20 +1,4 @@
-import 'dart:convert';
 import 'dart:typed_data';
-
-/// String utils
-extension StringX on String {
-  /// Indent string by spaces.
-  String indent({
-    int level = 2,
-    int start = 0,
-  }) {
-    final indentation = ' ' * level;
-    final lines = LineSplitter.split(this);
-    final unindentedLines = lines.take(start);
-    final indentedLines = lines.skip(start).map((line) => '$indentation$line');
-    return unindentedLines.followedBy(indentedLines).join('\n');
-  }
-}
 
 /// Uint8List utils
 extension Uint8ListX on Uint8List {
